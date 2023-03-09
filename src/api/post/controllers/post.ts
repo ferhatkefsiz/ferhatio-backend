38049bb3bdce5f8cd6da3a4598217ffb15293d21
ferhat kefsiz <ferhatkefsiz@yahoo.com>
2023-03-09 11:36:21 +0300

    feat: get Post Slug by id  >> slug

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -1,7 +1,27 @@
-/**
- * post controller
- */
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi'
+export default factories.createCoreController(
+  "api::post.post",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      console.log(ctx.params, ctx.query);
+      console.log(ctx.locale);
 
-export default factories.createCoreController('api::post.post');
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
+      const { locale = "en" } = query;
+
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+      query.filters.locale = { $eq: locale };
+      const entity = await strapi.service("api::post.post").find(query);
+
+      const { results } = await this.sanitizeOutput(entity, ctx);
+      console.log(results);
+
+      const result = results[0];
+
+      return this.transformResponse(result);
+    },
+  })
+);
